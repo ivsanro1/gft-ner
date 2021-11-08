7838f7dfc0d8fa71c6cb0c24b1f770bc3987d880
INSN <ivan.sanchez-rodriguez@gft.com>
2021-11-08 16:58:50 +0100

    Changed setup

--- setup.py
+++ setup.py
@@ -1,43 +1,37 @@
 import os
-import yaml
 from setuptools import setup, find_packages
-from pathlib import Path
 
-module_name = os.path.basename(Path(os.path.realpath(__file__)).parent)
-print('############ MODULE NAME: ')
-print(module_name)
-# requires_file = 'requires.txt'
+MODULE_NAME = 'ner'
 
-# cwd = Path(os.path.realpath(__file__)).parent
+requires_file = 'requires.txt'
+exec(f'from {MODULE_NAME} import VERSION')
 
-# with open(cwd / 'settings.yaml', 'r') as f:
-#   settings = yaml.load(f)
+try:
+  # Normal installation via setup.py
+  with open('requirements.txt') as f: 
+    requirements = f.read().splitlines()
+except FileNotFoundError:
+  # Happens if installed via pip of a packaged bdist (tar.gz when building wheel),
+  # because the packaged module has different structure, therefore we search for the
+  # requirements file. This works for setuptools==46.1.3
+  found_requires_path = None
+  for r, d, fs in os.walk('.', topdown=False):
+    for f in fs:
+      f_path = os.path.join(r, f)
+      if f_path.endswith(requires_file) and '.egg-info' in f_path:
+        found_requires_path = f_path
+  if (found_requires_path == None):
+    print('/!\ Requirements file not found /!\ ')
+  else:
+    with open(found_requires_path) as f: 
+      requirements = f.read().splitlines()
 
-# try:
-#   # Normal installation via setup.py
-#   with open('requirements.txt') as f: 
-#     requirements = f.read().splitlines()
-# except FileNotFoundError:
-#   # Happens if installed via pip of a packaged bdist (tar.gz when building wheel),
-#   # because the packaged module has different structure, therefore we search for the
-#   # requirements file. This works for setuptools==46.1.3
-#   found_requires_path = None
-#   for r, d, fs in os.walk('.', topdown=False):
-#     for f in fs:
-#       f_path = os.path.join(r, f)
-#       if f_path.endswith(requires_file) and '.egg-info' in f_path:
-#         found_requires_path = f_path
-#   if (found_requires_path == None):
-#     print('/!\ No requirements file found /!\ ')
-#   else:
-#     with open(found_requires_path) as f: 
-#       requirements = f.read().splitlines()
 
 setup(
-   name=module_name,
-   version='0.0.1',#settings['version'],
-   description='notebook stuff',
+   name=MODULE_NAME,
+   version=VERSION,
+   description='This library is for ...',
    packages=find_packages(),
-   #install_requires=requirements, # external packages as dependencies
-   include_package_data=True # settings.yaml specified in MANIFEST.in
+   install_requires=requirements, # external packages as dependencies
+   include_package_data=True # whatever is specified in MANIFEST.in
 )
